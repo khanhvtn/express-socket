93aac3ba62817ae37af558c75a27580ba390237f
khanhvtv <khanhvotrannhut93@gmail.com>
2021-04-06 17:31:47 +0700

    Finish api for creating user. Finish implement hash password for user login and create.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,4 @@
-
-
+const bcrypt = require('bcrypt')
 const { User } = require('../models')
 const jwt = require('jsonwebtoken')
 const userLogin = async (request, response) => {
@@ -10,7 +9,8 @@ const userLogin = async (request, response) => {
         if (!user) return response.status(400).json(new Error("Email doesn't exist."))
 
         //check password matching
-        if (password !== user.password) return response.status(403).json(new Error('Invalid Password.'))
+        const isValidPassword = await bcrypt.compare(password, user.password);
+        if (!isValidPassword) return response.status(403).json(new Error('Invalid Password.'))
 
         //if pass validation
         const token = jwt.sign({ id: user._id }, 'netcp', { expiresIn: '1h' });
@@ -22,7 +22,30 @@ const userLogin = async (request, response) => {
         response.status(400).json(new Error('Something went wrong.'))
     }
 }
+const userCreate = async (request, response) => {
+    const { email, password, role } = request.body
+    try {
+        //check email exists
+        const user = await User.findOne({ email })
+        if (user) return response.status(400).json(new Error("Email already exists."))
+
+        //hash password
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        //create user in database
+        const newUser = await User.create({
+            email,
+            password: hashPassword,
+            role,
+        })
+
+        response.status(200).json(newUser)
+    } catch (error) {
+        response.status(400).json(new Error('Something went wrong.'))
+    }
+}
 
 module.exports = userControllers = {
-    userLogin
+    userLogin,
+    userCreate
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const { userControllers } = require('../controllers')
 
 router.post('/login', userControllers.userLogin)
+router.post('/create', userControllers.userCreate)
 module.exports = router;
